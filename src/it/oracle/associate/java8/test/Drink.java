624ff09ad808565363aef14dcb000a90fba8f8f9
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2018-02-01 16:53:37 +0100

    Prove domande capitolo 6

---
+++ src/it/oracle/associate/java8/test/Drink.java
@@ -0,0 +1,10 @@
+package it.oracle.associate.java8.test;
+
+public class Drink {
+
+	public static void water() {}
+	
+	public void get() {
+		Drink.water();
+	}
+}
---
+++ src/it/oracle/associate/java8/test/Tree.java
@@ -0,0 +1,17 @@
+package it.oracle.associate.java8.test;
+
+public class Tree {
+	public final static long numberOfTrees;
+	public final double height;
+	
+	static {}
+	{
+		final int initHeight = 2;
+		height = initHeight;
+	}
+	
+	static {
+		numberOfTrees = 100;
+		//height = 4;
+	}
+}
